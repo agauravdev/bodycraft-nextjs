4a5d8103d83e1b434437d51672e3373ef496a504
Gaurav Agarwal <agauravdev@gmail.com>
2024-11-10 11:41:38 +0530

    basic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <>
       <div style={{ textAlign: 'center' }}>
-        <Image src="/logo.svg" alt="Bodycraft Aqua & Fitness Logo" width={150} height={150} />
+        <Image src="/favicon.svg" alt="Bodycraft Aqua & Fitness Logo" width={150} height={150} />
         <h1 style={{ margin: '20px 0', color: '#0B3D91' }}>Website is coming very soon.</h1>
       </div>
       <div style={{ position: 'absolute', bottom: '20px', left: '20px', color: '#0B3D91' }}>
